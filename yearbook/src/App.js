d3ddf5df9cfcafe9e6070588d4d666301241f14e
Khifayat Abolurin <kabolu1@students.towson.edu>
2021-04-28 15:44:48 -0400

    moved some pages around, page works the same

--- yearbook/src/App.js
+++ yearbook/src/App.js
@@ -1,56 +1,15 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Chat from "./components/chats";
-import Message from "./components/SendMessage"
+import Chatroom from './pages/chatroom'
+
+
 
 
 function App() {
 
-  var rooms = ["Towson Main", "Towson STEM", "GTJHS Main", "Towson Basketball"]
-  var messenger = ["Tom Smith", "Coach Andrews", "Alexis Matthews", "James Peters", "sam Hunter"]
   return (
-    <>
-    <div className="view">
 
-  
-      <Navbar/>
-      <div className = "row">
-
-       <div>
-        <Sidebar items = {rooms} title = "Chatroom"/>
-        <hr/>
-         <Sidebar   items = {messenger} title = "Direct messages"  />
-       </div>
-      
-      
-       <div className = "col">
-
-         <div className = "chats">
-         <Chat/>
-        <Chat/>
-        <Chat/>
-        <Chat/>
-        <Chat/>
-         <Chat/>
-        <Chat/>
-        <Chat/>
-        <Chat/>
-        <Chat/>
-        </div>
-
-        <div className = "message">
-        <Message/>
-        </div>
-
-
-       </div>
-        </div>
-
-
-        </div>
+    <Chatroom />
     
-    </>
   );
 }
 
---
+++ yearbook/src/pages/chatroom.js
@@ -0,0 +1,58 @@
+import "./chatroom.css";
+import Navbar  from "../components/Navbar";
+import Sidebar from '../components/Sidebar';
+import Chat from "../components/chats";
+import Message from "../components/SendMessage"
+
+
+function Chatroom(){
+  var rooms = ["Towson Main", "Towson STEM", "GTJHS Main", "Towson Basketball"]
+  var messenger = ["Tom Smith", "Coach Andrews", "Alexis Matthews", "James Peters", "sam Hunter"]
+return(
+ <>
+    <div className="view">
+
+  
+      <Navbar/>
+      <div className = "row">
+
+       <div>
+        <Sidebar items = {rooms} title = "Chatroom"/>
+        <hr/>
+         <Sidebar   items = {messenger} title = "Direct messages"  />
+       </div>
+      
+      
+       <div className = "col">
+
+         <div className = "chats">
+         <Chat/>
+        <Chat/>
+        <Chat/>
+        <Chat/>
+        <Chat/>
+        <Chat/>
+        <Chat/>
+        <Chat/>
+        <Chat/>
+        <Chat/>
+        </div>
+
+        <div className = "message">
+        <Message/>
+        </div>
+
+
+       </div>
+        </div>
+
+
+        </div>
+    
+    </>
+
+)
+
+}
+
+export default Chatroom;
